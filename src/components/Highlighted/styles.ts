e3c1081feee3b3144af4592290fc2d889c2ae28f
Arthur Diniz <arthurvdinizs@gmail.com>
2020-09-14 15:32:49 -0300

    feat(highlighted): color transition

--- src/components/Highlighted/styles.ts
+++ src/components/Highlighted/styles.ts
@@ -5,7 +5,7 @@ interface TextHighlightedProps {
 }
 
 export const TextHighlighted = styled.span<TextHighlightedProps>`
-  color: ${({ theme }) => theme.colors.background};
+  color: ${({ isHighlighted, theme }) => isHighlighted ? theme.colors.background : theme.colors.primary};
   padding: 0 10px;
 
   background-image: linear-gradient(
@@ -15,5 +15,5 @@ export const TextHighlighted = styled.span<TextHighlightedProps>`
   );
   background-size: 200%;
   background-position: ${({ isHighlighted }) => (isHighlighted ? '-100%' : 0)};
-  transition: background-position ${({ theme }) => theme.transitions.easeInOutQuadLong};
+  transition: background-position ${({ theme }) => theme.transitions.easeInOutQuadLong}, color ${({ theme }) => theme.transitions.easeInOutQuadLong};
 `
