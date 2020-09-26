b43642f56dc938cf328f7709d84994b7825ecb4d
Arthur Diniz <arthurvdinizs@gmail.com>
2020-09-26 20:27:29 -0300

    feat(home): added emoji and internal link with /

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home(): React.ReactElement {
       </Head>
       <Section>
         <Typography className="title" variant={TypographyVariant.Title}>
-          Hello!
+          Hello! 🤙
         </Typography>
         <Typography className="subtitle" variant={TypographyVariant.Subtitle}>
           I am <Highlighted>Arthur Diniz</Highlighted>,
@@ -32,7 +32,7 @@ export default function Home(): React.ReactElement {
           I am focusing on learning backend concepts and web development. I enjoy expressing myself through{' '}
           <Highlighted>photography, art and code</Highlighted> that's why I created my{' '}
           <LinkText variant={LinkTextVariant.Internal} href="/lab">
-            laboratory
+            /laboratory
           </LinkText>{' '}
           for experimental projects.
         </Typography>
