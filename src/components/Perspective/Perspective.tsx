9e17796e4934217039cf48d606bfbfa6ed8e3b7c
Arthur Diniz <arthurvdinizs@gmail.com>
2020-09-30 20:49:42 -0300

    feat(perspective): change lib

--- src/components/Perspective/Perspective.tsx
+++ src/components/Perspective/Perspective.tsx
@@ -1,9 +1,5 @@
 import React from 'react'
 
-import { useSpring } from 'react-spring'
-
-import getXYS from './utils/getXYS'
-import getTransform from './utils/getTransform'
 import { Animated } from './styles'
 
 interface PerspectiveProps {
@@ -13,15 +9,8 @@ interface PerspectiveProps {
 function Perspective(props: PerspectiveProps): React.ReactElement {
   const { children } = props
 
-  const [sprops, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
-
   return (
-    <Animated
-      onMouseMove={({ clientX: x, clientY: y }) => set({ xys: getXYS(x, y) })}
-      onMouseLeave={() => set({ xys: [0, 0, 1] })}
-      // @ts-ignore
-      style={{ transform: sprops.xys.interpolate(getTransform) }}
-    >
+    <Animated drag dragConstraints={{ top: -50, left: -50, right: 50, bottom: 50 }} whileHover={{ scale: 1.1 }}>
       {children}
     </Animated>
   )
